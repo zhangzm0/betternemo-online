498fab124ac1a168a43c2caa8299629fb829db05
HHCL233 <XINGTAISHIJIAOXIQU@outlook.com>
2026-05-04 22:44:49 +0800

    feat: 新增顶栏,更换字体

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <RouterView />
+  <mdui-layout>
+    <mdui-top-app-bar class="bn-online-top-app-bar" scroll-behavior="elevate" scroll-threshold="0">
+      <mdui-dropdown>
+        <mdui-button-icon icon="menu" slot="trigger"></mdui-button-icon>
+        <mdui-menu>
+          <mdui-menu-item>新建作品</mdui-menu-item>
+          <mdui-menu-item>打开作品</mdui-menu-item>
+          <mdui-divider></mdui-divider>
+          <mdui-menu-item>关于 BetterNemo</mdui-menu-item>
+          <mdui-menu-item>关于 BetterNemo-Online</mdui-menu-item>
+        </mdui-menu>
+      </mdui-dropdown>
+      <mdui-top-app-bar-title>BetterNemo-Online</mdui-top-app-bar-title>
+    </mdui-top-app-bar>
+    <RouterView />
+  </mdui-layout>
 </template>
 
-<style scoped></style>
+<style scoped>
+@media (max-width: 768px) {
+  .bn-online-top-app-bar {
+    display: none;
+  }
+}
+</style>
--- src/pages/EditorPage.vue
+++ src/pages/EditorPage.vue
@@ -1,29 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="editor">
-    <mdui-navigation-rail contained divider value="code">
-      <mdui-button-icon icon="menu" slot="top"></mdui-button-icon>
-      <mdui-navigation-rail-item icon="code" value="code"></mdui-navigation-rail-item>
-      <mdui-navigation-rail-item icon="people" value="actor"></mdui-navigation-rail-item>
-      <mdui-button-icon icon="play_arrow" slot="bottom" variant="filled"></mdui-button-icon>
-    </mdui-navigation-rail>
-    <div class="bn">
-      <h1>占位符</h1>
-    </div>
-  </div>
+  <mdui-navigation-rail contained divider value="code" class="navigation-rail">
+    <mdui-navigation-rail-item icon="code" value="code"></mdui-navigation-rail-item>
+    <mdui-navigation-rail-item icon="people" value="actor"></mdui-navigation-rail-item>
+    <mdui-button-icon icon="play_arrow" slot="bottom" variant="filled"></mdui-button-icon>
+  </mdui-navigation-rail>
+  <mdui-layout-main class="bn">
+    <h1>占位符</h1>
+  </mdui-layout-main>
 </template>
 
 <style scoped>
-.editor {
-  display: flex;
-  height: 100%;
-  width: 100%;
-  padding-left: 0px !important;
+.navigation-rail {
+  z-index: 0;
 }
 
 .bn {
-  margin: 0;
   height: 100vh;
 }
 </style>
